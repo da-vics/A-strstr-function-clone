5ea541d42e938f33cc8b000b2a3a466d4244c6c2
Tomoloju Victor <tomoloju45@gmail.com>
2018-07-12 00:37:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,100 +4,70 @@
 
 using namespace std;
 
+char *dis_find(const char *t , const char *a){    /// function......
 
-
-///vector<char>gg;
-
-char *dis_find(const char *t , const char *a){
-
-int init(0),
- temp(0) ,
+unsigned init(0),
+ temp(0) ,         /// function variables....
  count_t(0);
 
-for(int i = 0; i<strlen(t);++i){
+ for(int i = 0 ;i!=strlen(t); ++i){
 
     if(a[init] == t[i]){
 
-        temp = i;
+        if(init == 0){
+            temp = i;     /// assigns a starting point....
+        }
+
         ++init;
         ++count_t;
+    }
 
-    for(int x = temp+1 ;init!=strlen(a); ++x){
+    else{
+        init = 0;
+        count_t = 0;   
+    }
+
+    if(init == strlen(a)){
+        break;
+    }
+
+    if(t[i] == ' '  || i == strlen(t)-1 && init!=strlen(a)){
+
+        init = 0;
+        count_t =0;       //// reset values under the above conditons
+    }
+
+ }  /// end of main for loop....
+
+ if(init>0){
+
+    size_t get_size = strlen(t) - temp;
+
+    char *point_char = nullptr;
+    point_char = new char[get_size];   /// dynamic memory allocation....
 
 
-        if(a[init] == t[x]){
-            ++init;
-            ++count_t;
+    for(int i = temp , che = 0; i<strlen(t) ; ++i,++che){
+
+        point_char[che] = t[i];
         }
 
-        else{
-            count_t = 0;
-    break;
-        }
+    return point_char;
+    delete [] point_char;  /// free memory
 
-
-    } /// end....
-
-    ///tt= init; test
-
-    break;
-    }  ///....end
-}
- ///end of ist for....
-
-if(count_t >= 1){
-
-
-
- int che(0);
-/*
- size_t of_temp = strlen(t) - temp;
-
- char temp_char[of_temp];
- for(int p = temp; che!= of_temp; ++p,++che){
-
-    temp_char[che] = t[p];
-    ///gg.push_back(t[p]);
  }
-temp_char[of_temp] = '\0';
 
-char *oget = nullptr;
-oget = temp_char;
-
- return oget;
- */
-  size_t of_temp = strlen(t) - temp;
-
- char *temp_ptr = nullptr;
-
- temp_ptr = new char[of_temp];
-
-  for(int p = temp; che!= of_temp; ++p,++che){
-
- temp_ptr[che] = t[p];
-}
-
-
-return temp_ptr;
-delete [] temp_ptr;
-
-}/// if ends....
-else{
+ else{
     return nullptr;
-}
+ }
 
-}
-
+}///..
 
 int main()
 {
+   char vic[] = "c++ programmers are awesome";
 
-
-
-   char vic[] = "victor";
-
-   cout<<dis_find(vic,"c");
-
-
-
+   cout<<dis_find(vic,"c++")<<endl;  /// cstring clone...ish....
+ 
+cout<<strstr(vic,"c++");     /// the cstring function.... 
 }
